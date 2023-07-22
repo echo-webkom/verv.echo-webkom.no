040ed376e1bea6d04f35e43f2fc9dec68f4f14aa
Ole Magnus Fon Johnsen <me@omfj.no>
2023-07-22 11:05:25 +0200

    Update migrations

---
+++ drizzle/migrate.ts
@@ -0,0 +1,14 @@
+import { migrate } from 'drizzle-orm/postgres-js/migrator';
+import { db } from '../src/lib/db/drizzle';
+
+console.log('🚀 Starting migrations...');
+
+migrate(db, { migrationsFolder: './drizzle/migrations' })
+	.then(() => {
+		console.log('✅ Migrations complete!');
+		process.exit(0);
+	})
+	.catch((err) => {
+		console.error('❌ Migrations failed! Error:', err);
+		process.exit(1);
+	});
---
+++ drizzle/migrations/0001_parched_black_knight.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS "profiles" (
+	"id" uuid PRIMARY KEY NOT NULL,
+	"is_webkom" boolean DEFAULT false NOT NULL
+);

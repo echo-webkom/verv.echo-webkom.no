7129fcee36c139e903b1d7ea5f78df8be3b62bbb
Ole Magnus Fon Johnsen <me@omfj.no>
2023-07-22 22:26:37 +0200

    Update theme color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -33,7 +33,7 @@
 	<link rel="manifest" href="/site.webmanifest" />
 	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
 	<meta name="msapplication-TileColor" content="#2b5797" />
-	<meta name="theme-color" content="#ffffff" />
+	<meta name="theme-color" content="#0d0e0e" />
 </svelte:head>
 
 <Toaster theme="dark" />

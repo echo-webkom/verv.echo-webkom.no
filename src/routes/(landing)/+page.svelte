e2053525f616dfcb4ec394ca9958956a3e58d2aa
torgerbocianowski <torgerboc@gmail.com>
2023-07-22 19:16:46 +0200

    Update text and layout

--- src/routes/(landing)/+page.svelte
+++ src/routes/(landing)/+page.svelte
@@ -3,7 +3,7 @@
 	import Logo from '$lib/assets/webkom-logo.png';
 </script>
 
-<main class="flex flex-col py-10 sm:py-24 px-3 transition-all duration-150">
+<main class="flex flex-col pt-10 sm:pt-24 px-3 transition-all duration-150">
 	<div class="text-center py-24">
 		<img class="w-40 h-auto mx-auto" src={Logo} alt="Webkom-logo" />
 		<h1 class="text-5xl font-bold py-10">Søk verv i echo Webkom</h1>
@@ -13,7 +13,7 @@
 		>
 	</div>
 
-	<section class="py-10 max-w-2xl mx-auto w-full">
+	<section class="pt-10 pb-4 max-w-2xl mx-auto w-full">
 		<article class="space-y-4 text-lg">
 			<h2 class="text-xl font-semibold">Om oss</h2>
 
@@ -44,22 +44,22 @@
 			<h2 class="text-xl font-semibold pt-4">Hvorfor deg?</h2>
 
 			<p>
-				Vi ser etter motiverte, engasjerte og hyggelige folk! Relevant erfaring er selvsagt et
-				pluss, men ikke et krav.
+				Du er motivert, engasjert og ikke minst hyggelig! Relevant erfaring er selvsagt et pluss,
+				men ikke et krav.
 			</p>
 
 			<p>
-				Så lenge du er en lagspiller, åpen for å lære og utvikle deg, og at du er engasjert i å
-				bidra til å gjøre echo til et bedre sted for alle studenter.
+				Du er en lagspiller, åpen for å lære og utvikle deg, og du er engasjert i å bidra til å
+				gjøre echo til et bedre sted for alle.
 			</p>
 
-			<h2 class="text-xl font-semibold pt-8">Kjenner du deg igjen? — Søk da vel!</h2>
+			<h2 class="text-xl font-semibold pt-10">Kjenner du deg igjen? — Søk da vel!</h2>
 
-			<Button>
+			<Button class="w-full">
 				<a class="text-white hover:no-underline" href="/apply"> Søk her </a>
 			</Button>
 
-			<h2 class="text-xl font-semibold">Kontakt oss</h2>
+			<h2 class="text-xl font-semibold pt-28">Kontakt oss</h2>
 
 			<p>
 				Har du spørsmål om vervet eller Webkom generelt? Ta kontakt med oss på <a

3a509ac5e7ab800c41f944b544f84712c7142cce
Ole Magnus Fon Johnsen <me@omfj.no>
2023-07-23 12:06:22 +0200

    Update SEO

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,17 +25,6 @@
 	});
 </script>
 
-<svelte:head>
-	<title>Verv | echo Webkom</title>
-	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-	<link rel="manifest" href="/site.webmanifest" />
-	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-	<meta name="msapplication-TileColor" content="#2b5797" />
-	<meta name="theme-color" content="#0d0e0e" />
-</svelte:head>
-
 <Toaster theme="dark" />
 
 <div class="min-h-screen flex flex-col gap-6">
--- vite.config.ts
+++ vite.config.ts
@@ -7,31 +7,6 @@ export default defineConfig({
 		sveltekit(),
 		SvelteKitPWA({
 			registerType: 'autoUpdate',
-			disable: process.env.NODE_ENV === 'development',
-			manifest: {
-				name: 'verv - echo Webkom',
-				short_name: 'verv - echo Webkom',
-				start_url: 'https://verv.echo-webkom.no',
-				lang: 'nb',
-				orientation: 'any',
-				dir: 'ltr',
-				scope: '/',
-				icons: [
-					{
-						src: '/android-chrome-192x192.png',
-						sizes: '192x192',
-						type: 'image/png'
-					},
-					{
-						src: '/android-chrome-512x512.png',
-						sizes: '512x512',
-						type: 'image/png'
-					}
-				],
-				theme_color: '#0d0e0e',
-				background_color: '#0d0e0e',
-				display: 'standalone'
-			}
 		})
 	]
 });

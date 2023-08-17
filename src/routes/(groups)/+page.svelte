074633be5ff47d173e43daa593775589cf17691b
torgerbocianowski <torgerboc@gmail.com>
2023-08-17 19:29:29 +0200

    Grid view on large screen

--- src/routes/(groups)/+page.svelte
+++ src/routes/(groups)/+page.svelte
@@ -10,15 +10,14 @@
 	}));
 </script>
 
-<img class="w-40 h-auto mx-auto" src={Logo} alt="Webkom-logo" />
-
+<img class="w-40 h-auto mx-auto hover:scale-110 transition-transform duration-300 ease-in" src={Logo} alt="Webkom-logo" />
 
 <h2 class="text-4xl font-bold text-center mb-12">Søk verv i echo i dag!</h2>
 
-<ul class="space-y-4">
+<ul class="grid gap-6 max-w-xl mx-auto lg:grid-cols-3 lg:max-w-6xl lg:gap-10">
 	{#each groups as { title, value }}
 		<li>
-			<a href="/{value}" class="px-3 py-5 border flex flex-col gap-2 rounded-md group hover:bg-gray-900 transition-colors duration-200">
+			<a href="/{value}" class="px-3 py-5 h-28 border flex flex-col gap-2 rounded-md group hover:bg-gray-950 transition-colors duration-200">
 				<h2 class="text-xl font-bold group-hover:underline">{title}</h2>
 
 				<p>Klikk her for å søke verv i {title}</p>

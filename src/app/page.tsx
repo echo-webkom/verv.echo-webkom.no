25e9bc8f3342d5dfb4d3124a48569df9b11417e8
Ole Magnus Fon Johnsen <me@omfj.no>
2024-05-15 18:19:22 +0200

    Initialize next-app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,112 @@
-import { ChevronDownIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
-
-const groups = [
-  {
-    to: "/webkom",
-    name: "💻 Webkom",
-  },
-  {
-    to: "/gnist",
-    name: "✨ Gnist",
-  },
-];
 
 export default function Home() {
   return (
-    <main className="space-y-4 max-w-2xl w-full mx-auto px-6">
-      <div className="text-center flex flex-col gap-4 pt-12 pb-24">
-        <Image
-          src="/images/echo-logo.png"
-          className="mx-auto"
-          width={200}
-          height={200}
-          alt="echo logo"
-          quality={100}
-        />
-
-        <h1 className="text-4xl md:text-5xl font-bold">
-          Søk verv i Webkom eller Gnist!
-        </h1>
-
-        <p className="text-lg">
-          Echo har nå åpnet for søknader til verv i noen av gruppene våre.
-          Søknadsfristen er 14. februar. Det er lov å søke på begge gruppene!
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">src/app/page.tsx</code>
         </p>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{" "}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
       </div>
 
-      <div>
-        <div className="mb-2">
-          <h2 className="text-3xl font-bold text-center mb-2">
-            Våre undergrupper
+      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Docs{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
           </h2>
-          <p className="text-center text-gray-700">
-            Trykk på en av undergruppene for å lære mer.
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Find in-depth information about Next.js features and API.
           </p>
-        </div>
+        </a>
 
-        <ChevronDownIcon className="mx-auto animate-bounce h-6 w-6" />
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Learn{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          </p>
+        </a>
 
-        <ul className="grid grid-cols-2 place-items-center">
-          {groups.map(({ to, name }) => {
-            const emoji = name.split(" ")[0];
-            const title = name.split(" ")[1];
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Templates{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Explore starter templates for Next.js.
+          </p>
+        </a>
 
-            return (
-              <li key={name} className="py-6 flex flex-row items-center">
-                <a href={to} className="flex-1">
-                  <h2 className="flex flex-col text-2xl font-bold">
-                    <span className="flex justify-center group-hover:underline ml-2">
-                      {title}
-                    </span>
-                    {title === "Webkom" ? (
-                      <span className="flex justify-center group-hover:underline">
-                        <Image
-                          src="/images/webkom-logo.png"
-                          width={70}
-                          height={70}
-                          alt="webkom logo"
-                          quality={100}
-                        />
-                      </span>
-                    ) : (
-                      <span className="flex justify-center text-5xl">
-                        {emoji}
-                      </span>
-                    )}
-                    <span
-                      className="flex justify-center text-5xl py-2"
-                      aria-hidden="true"
-                    ></span>
-                    <span className="flex justify-center">
-                      <a href={to}>
-                        <ChevronDownIcon className="flex mx-auto animate-bounce h-4 w-4" />
-                      </a>
-                    </span>
-                  </h2>
-                </a>
-              </li>
-            );
-          })}
-        </ul>
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Deploy{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
       </div>
     </main>
   );
--- src/lib/config.ts
+++
@@ -1,4 +0,0 @@
-export const baseURL =
-  process.env.NODE_ENV === "production"
-    ? "https://beta.echo-webkom.no"
-    : "http://localhost:3000";

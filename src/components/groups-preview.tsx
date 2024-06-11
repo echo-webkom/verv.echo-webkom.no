875f64b06407bda7219edaab3d991eb9a5751c22
Izaak Sarnecki <izaaksarnecki@outlook.com>
2024-06-11 15:45:29 +0200

    why are so many things said to be modified

--- src/components/groups-preview.tsx
+++ src/components/groups-preview.tsx
@@ -2,20 +2,33 @@ import { db } from "@/server/db/drizzle";
 import { workspaces } from "@/server/db/schemas";
 
 export const GroupsPreview = async () => {
-  const result = await db.select().from(workspaces);
+  const result = await (await db.select().from(workspaces));
 
   return (
     <div className="mx-auto w-1/2">
       <p>Sjekk ut undergruppene våres her!</p>
-      <ul>
+      <table className=" border-2 bg-blue-200">
+        <tbody>
+          {result.map((workspace) => (
+            <tr key={workspace.id}>
+              <td className=" border-2">{workspace.name}</td>
+              <td className=" border-2">{workspace.description}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+     
+      {/* <ul>
         {result.length > 0 ? (
           result.map((workspace) => (
-            <li key={workspace.id}>{workspace.name}</li>
+            <div>
+              <li key={workspace.id}>{workspace.name} {workspace.description}</li>
+            </div>
           ))
         ) : (
           <li>No workspaces found</li>
         )}
-      </ul>
+      </ul> */}
     </div>
   );
 };

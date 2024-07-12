9211135d508aff18f436274bbbf029351d03be2c
Ole Magnus Fon Johnsen <me@omfj.no>
2024-07-12 18:14:31 +0200

    Add ability to not use turso

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,17 +2,30 @@ import "dotenv/config";
 
 import { defineConfig } from "drizzle-kit";
 
-if (!process.env.DATABASE_URL) {
+const DATABASE_URL = process.env.DATABASE_URL;
+const DATABASE_AUTH_TOKEN = process.env.DATABASE_AUTH_TOKEN || undefined;
+
+if (!DATABASE_URL) {
   throw new Error("DATABASE_URL is not set");
 }
 
+const isHttpProxy = DATABASE_URL.startsWith("http") || DATABASE_URL.startsWith("libsql");
+
 export default defineConfig({
-  driver: "turso",
   dialect: "sqlite",
-  dbCredentials: {
-    url: process.env.DATABASE_URL,
-    authToken: process.env.DATABASE_AUTH_TOKEN,
-  },
   out: "./migrations",
   schema: "./src/server/db/schemas/index.ts",
+  ...(isHttpProxy
+    ? {
+        driver: "turso",
+        dbCredentials: {
+          url: DATABASE_URL,
+          authToken: DATABASE_AUTH_TOKEN,
+        },
+      }
+    : {
+        dbCredentials: {
+          url: DATABASE_URL,
+        },
+      }),
 });

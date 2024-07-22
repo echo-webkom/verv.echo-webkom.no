cada86aa25e59be29017067b240cd292425e97a1
Izaak Sarnecki <izaaksarnecki@outlook.com>
2024-07-22 15:11:15 +0200

    start list

--- src/components/workspace-display.tsx
+++ src/components/workspace-display.tsx
@@ -1,9 +1,17 @@
 "use client";
 
-export const WorkspaceDisplay = () => {
-    return (
-        <div>
-            hello
-        </div>
-    )
+const Cards = () => {
+  
+  return (
+    <div></div>
+  ); 
 }
+
+export const WorkspaceDisplay = () => {
+  return (
+    <div className="flex space-x-2">
+      <div className="w-1/4 bg-blue-500 rounded-sm pl-2">Test 1</div>
+      <div className="w-3/4 bg-green-500 rounded-sm pl-2">Test 2</div>
+    </div>
+  );
+};

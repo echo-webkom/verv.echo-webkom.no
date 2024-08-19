4a1076967300a83c70121bd65bd52806f2cc9106
Kristian Elde Johansen <kristian.elde.johansen@gmail.com>
2024-08-19 22:08:40 +0200

    Added custom form for bedkom

--- src/app/sok/[group]/_components/application-form.tsx
+++ src/app/sok/[group]/_components/application-form.tsx
@@ -4,6 +4,7 @@ import { AuthUser } from "@/lib/auth/lucia";
 import { Group } from "@/lib/constants";
 import { GeneralApplication } from "./general-application";
 import { WebkomApplication } from "./webkom-application";
+import { BedkomApplication } from "./bedkom-application";
 
 export type ApplicationFormProps = {
   group: Group;
@@ -14,6 +15,8 @@ export const ApplicationForm = (props: ApplicationFormProps) => {
   switch (props.group) {
     case "webkom":
       return <WebkomApplication {...props} />;
+    case "bedkom":
+      return <BedkomApplication {...props} />;
     default:
       return <GeneralApplication {...props} />;
   }

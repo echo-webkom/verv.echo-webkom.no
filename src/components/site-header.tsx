b10f9f17e8fc0c5cf108c10f1b0dbcfbaa380671
Ole Magnus Fon Johnsen <me@omfj.no>
2024-09-06 23:57:42 +0200

    Vår -> Høst

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -19,7 +19,7 @@ export async function SiteHeader() {
         {new Date() > APPLICATION_DEADLINE && (
           <div className="bg-[#ff9b9b] py-2">
             <p className="text-center text-sm font-bold">
-              Søknadsfristen for vårsemesteret 2024 har gått ut.
+              Søknadsfristen for høstsemesteret 2024 har gått ut.
             </p>
           </div>
         )}

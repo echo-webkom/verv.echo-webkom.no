17196878f9e7287b68bf660c22817771631d2fe7
Ole Magnus Fon Johnsen <me@omfj.no>
2025-05-23 17:42:09 +0200

    Remove background from button

--- src/app/logg-inn/login-button.tsx
+++ src/app/logg-inn/login-button.tsx
@@ -4,10 +4,7 @@ import Link from "next/link";
 
 export const LoginButton = () => {
   return (
-    <Link
-      href="/auth/feide"
-      className="group mx-auto block w-full max-w-sm border-2 border-black bg-white"
-    >
+    <Link href="/auth/feide" className="group mx-auto block w-full max-w-sm border-2 border-black">
       <span className="bg-secondary group-hover:bg-secondary relative -top-2 -right-2 block border-2 border-black px-4 py-1 text-center font-semibold transition-all duration-200 group-hover:-top-1 group-hover:-right-1">
         Logg inn
       </span>
